3eda8f9b39bf2ecd28969d56718fbbf92fd4b636
Asongna-frank <asongnafrank@gmail.com>
2023-07-16 05:45:54 +0100

    did numbers exercise

---
+++ DS_python_code/3_numbers_exercise.py
@@ -0,0 +1,28 @@
+# TASK 1
+length = 92
+width = 48.8
+area = length * width
+print('Area =:', area)
+
+# TASK 2
+packets_of_chips = 9
+cost_per_packet = 1.49
+amount_paid = 20
+balance = amount_paid - (packets_of_chips * cost_per_packet)
+print('Balance =' + " $" + str(balance))
+
+# TASK 3
+length_square = 5.5
+cost_per_square_feet = 500
+area_square = length_square ** 2
+total_cost = area_square * cost_per_square_feet
+print('Total cost =', total_cost)
+
+# TASK 4
+num = 17
+rem = 0
+while num > 0:
+    num = num / 2
+    rem = rem + (num % 2)
+str(rem)
+print('The binary of', num, "is", rem[::-1])
